b68c03a231536034e4b0f3f322bfdbc78788c634
Abdulrahman <abd.alrahmanh.1995@gmail.com>
2021-01-10 22:58:37 +0200

    - Use PolySymbol for injection.

--- src/injectionSymbols.ts
+++ src/injectionSymbols.ts
@@ -2,7 +2,6 @@ export const hasSymbol =
   typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol';
 
 export const PolySymbol = (name: string) =>
-  // vr = vue router
   hasSymbol ? Symbol(name) : name;
 
 export const gtmKey = PolySymbol('gtmPlugin');
