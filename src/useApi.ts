349fe11ca763b908064e9dd884339a8a1f07615d
Abdulrahman <abd.alrahmanh.1995@gmail.com>
2021-01-10 23:51:59 +0200

    - Log.

--- src/useApi.ts
+++ src/useApi.ts
@@ -5,12 +5,18 @@ import { gtmKey } from "./injectionSymbols";
 /**
  * Returns gtm plugin to be used via composition api inside setup method
  */
-export function useGtm(): {gtmPlugin: Function} {
+export function useGtm(): {gtmPlugin: Function, gtmConfig: Function} {
   function gtmPlugin() {
     const gtmInstance: GtmPlugin | undefined = inject(gtmKey);
     console.log('gtmInstance', gtmInstance);
     return gtmInstance;
   }
 
-  return {gtmPlugin};
+  function gtmConfig() {
+    const gtmConfig = inject('gtm');
+    console.log('gtmConfig', gtmConfig);
+    return gtmConfig;
+  }
+
+  return {gtmPlugin, gtmConfig};
 }
